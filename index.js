2e6015479d8e69f66fddd84342e92fd4b6e1aebd
Naimur Reza <naimur.rezaa@gmail.com>
2023-05-29 19:45:45 +0600

    installed jwt and .env fixed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
@@ -11,7 +12,7 @@ app.get("/", (req, res) => {
 });
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri = `mongodb+srv://Bistro-Boss:ccicrWaxVWb3uIi7@cluster0.2cofc5d.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2cofc5d.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -59,11 +60,43 @@ async function run() {
     });
 
     // user data base
+
+    // get all users
+    app.get("/users", async (req, res) => {
+      const result = await usersDb.find().toArray();
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const userData = req.body;
+      const query = { email: userData.email };
+      const existingUser = await usersDb.findOne(query);
+      if (existingUser) {
+        console.log("existing User", existingUser);
+        return res.send({ message: "User already exist" });
+      }
       const result = await usersDb.insertOne(userData);
       res.send(result);
     });
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersDb.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.delete("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await usersDb.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
